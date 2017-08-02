1078bc15baedc799a3eb2cb356f26eb392194098
chris.holliday@gmail.com <chris.holliday@gmail.com>
2017-08-02 11:36:42 -0500

    made the oil sysmbol be the 'next' month

--- src/components/StockList.vue
+++ src/components/StockList.vue
@@ -3,7 +3,8 @@
   p.title.is-2 Stock List
   .columns.is-multiline
       kpi-tile(v-bind:trendTicker='"COM/COFFEE_CLMB"' v-bind:ticker='"KC=F"' v-bind:title='"Coffee Futures"' )
-      kpi-tile(v-bind:trendTicker='"FRED/DCOILWTICO"' v-bind:ticker='"CLJ17.NYM"' v-bind:title='"WTI Crude Price"' )
+      //- kpi-tile(v-bind:trendTicker='"FRED/DCOILWTICO"' v-bind:ticker='"CLJ17.NYM"' v-bind:title='"WTI Crude Price"' )
+      kpi-tile(v-bind:trendTicker='"FRED/DCOILWTICO"' v-bind:ticker='"CL=F"' v-bind:title='"WTI Crude Price"' )
       kpi-tile(v-bind:trendTicker='"BITSTAMP/USD"' v-bind:ticker='"BTCUSD=X"')
       kpi-tile(v-bind:trendTicker='"FED/RXI_N_B_IN"' v-bind:ticker='"USDINR=X"' v-bind:title='"Rupee/USD Exchange Rate"' )
 </template>
